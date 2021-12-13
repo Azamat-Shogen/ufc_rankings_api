ed68f75f699da01dd15861f08e44a583340571f7
Azamat Shogen <baki47hanma@gmail.com>
2021-12-12 22:52:45 -0600

    created django app

---
+++ app/ufc_rankins/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ufc_rankins project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ufc_rankins.settings')
+
+application = get_asgi_application()
