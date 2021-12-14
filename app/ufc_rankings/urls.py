940e6978f3cb0d5246e489378261b8dd75f04248
Azamat Shogen <baki47hanma@gmail.com>
2021-12-14 07:18:46 -0600

    renamed_folder

--- app/ufc_rankings/urls.py
+++ app/ufc_rankings/urls.py
@@ -1,4 +1,4 @@
-"""ufc_rankins URL Configuration
+"""ufc_rankings URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
